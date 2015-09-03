bf62e758a4ccbed2eb5794e54e4b0c57a49304fe
Tessa <Tessachi33@yahoo.com>
2015-09-03 13:11:11 -0700

    pass emptyAtFirst unit test for Store.java

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,3 +1,57 @@
 import java.util.List;
 import org.sql2o.*;
 import java.util.ArrayList;
+
+public class Store {
+  private int id;
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Store(String name) {
+    this.name = name;
+  }
+
+  public static List<Store> all() {
+    String sql = "SELECT id, name FROM Stores";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Store.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherStore){
+    if (!(otherStore instanceof Store)) {
+      return false;
+    } else {
+      Store newStore = (Store) otherStore;
+      return this.getName().equals(newStore.getName());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO Stores(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Store find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM Stores where id=:id";
+      Store Store = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Store.class);
+      return Store;
+    }
+  }
+}
--- src/test/java/StoreTest.java
+++ src/test/java/StoreTest.java
@@ -1,16 +1,38 @@
-import java.util.Arrays;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Arrays;
+
 
 public class StoreTest {
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
-  // 
-  // @Test
-  // public void all_emptyAtFirst() {
-  //   assertEquals(Store.all().size(), 0);
-  // }
 
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Store.all().size(), 0);
+  }
+
+  // @Test
+  // public void equals_returnsTrueIfNamesAretheSame() {
+  //   Store firstStore = new Store("The mall");
+  //   Store secondStore = new Store("The mall");
+  //   assertTrue(firstStore.equals(secondStore));
+  // }
+  //
+  // @Test
+  // public void save_savesIntoDatabase_true() {
+  //   Store myStore = new Store("The mall");
+  //   myStore.save();
+  //   assertTrue(Store.all().get(0).equals(myStore));
+  // }
+  //
+  // @Test
+  // public void find_findStoreInDatabase_true() {
+  //   Store myStore = new Store("The mall");
+  //   myStore.save();
+  //   Store savedStore = Store.find(myStore.getId());
+  //   assertTrue(myStore.equals(savedStore));
+  // }
 }
